0e6c67bbb6493c4a966b8d7a0af779ace7e10988
tagaibek <adyl2692@gmail.com>
2019-10-13 21:29:24 -1100

    Spring security finely

--- main/java/com/javamaster/config/WebAppInitializer.java
+++ main/java/com/javamaster/config/WebAppInitializer.java
@@ -6,19 +6,16 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        // TODO Auto-generated method stub
-        return new Class[]{AppConfig.class};
+        return new Class[]{AppConfig.class, WebSecurityConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        // TODO Auto-generated method stub
         return new Class[]{WebConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        // TODO Auto-generated method stub
         return new String[]{"/"};
     }
 
--- main/java/com/javamaster/config/WebSecurityConfig.java
+++ main/java/com/javamaster/config/WebSecurityConfig.java
@@ -1,39 +1,52 @@
 package com.javamaster.config;
 
-import com.javamaster.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@ComponentScan(basePackages = "com.javamaster")
+@ComponentScan("com.javamaster.config")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private CustomUserDetailsService userDetailsService;
+    private CustomizeAuthenticationSuccessHandler customizeAuthenticationSuccessHandler;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @SuppressWarnings("deprecation")
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/admin/*").authenticated()
-                .anyRequest().permitAll()
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/admin/users").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
                 .and()
-                .formLogin()
-                .and()
-                .logout();
+                .formLogin().successHandler(customizeAuthenticationSuccessHandler);
     }
 }
